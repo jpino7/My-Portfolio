ac4cbaed45a6d89691574314660027392fca1264
JRPino <pino3789@gmail.com>
2020-07-01 18:27:44 -0700

    Updates

---
+++ precache-manifest.b15b6875d1944a67de19eed62bdc8422.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbf2b7277188beb840a2e4d9e38f7b3",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "f510c3770bd5d8dae63d",
+    "url": "/My-Portfolio/static/css/main.6c174a1b.chunk.css"
+  },
+  {
+    "revision": "d56aac489a1fa6314423",
+    "url": "/My-Portfolio/static/js/2.fcae1c39.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/My-Portfolio/static/js/2.fcae1c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f510c3770bd5d8dae63d",
+    "url": "/My-Portfolio/static/js/main.13fa5157.chunk.js"
+  },
+  {
+    "revision": "cc8fb5c09ef196e1df09",
+    "url": "/My-Portfolio/static/js/runtime-main.2dca773a.js"
+  },
+  {
+    "revision": "33040bbe2554f392bacf043244e73347",
+    "url": "/My-Portfolio/static/media/BudgetTracker.33040bbe.png"
+  },
+  {
+    "revision": "b38e3f1864abb43c5465e245fb19f747",
+    "url": "/My-Portfolio/static/media/CodeQuiz.b38e3f18.png"
+  },
+  {
+    "revision": "4ea645368ac7e758e8378ab943664953",
+    "url": "/My-Portfolio/static/media/Demo.gif.4ea64536.gif"
+  },
+  {
+    "revision": "839961804a5532890c4d5551dcc35958",
+    "url": "/My-Portfolio/static/media/EatDaBurger.83996180.png"
+  },
+  {
+    "revision": "f98fff1efe5be80b442e50846c41ef11",
+    "url": "/My-Portfolio/static/media/LaughyTasky.f98fff1e.png"
+  },
+  {
+    "revision": "f80ec8519739e1ae0b49d12b684f5080",
+    "url": "/My-Portfolio/static/media/LeadAstray.f80ec851.png"
+  },
+  {
+    "revision": "660359376118c214ac1c3b7c6f8409ad",
+    "url": "/My-Portfolio/static/media/Pic.66035937.jpg"
+  },
+  {
+    "revision": "10b38b0241eb7e22bb373279e5bc09ba",
+    "url": "/My-Portfolio/static/media/TeamGenerator.10b38b02.png"
+  },
+  {
+    "revision": "502416e9f71bdfa798297dda00d8d725",
+    "url": "/My-Portfolio/static/media/WeatherorNot.502416e9.png"
+  },
+  {
+    "revision": "ad725b374b6bd0cb33dd3fea7d8ad606",
+    "url": "/My-Portfolio/static/media/dark-ocean.ad725b37.jpg"
+  },
+  {
+    "revision": "406fee0d2ac4a7b4dc6f77230abe50ba",
+    "url": "/My-Portfolio/static/media/jpaesthiticOfficialLogo.406fee0d.jpg"
+  },
+  {
+    "revision": "f7f33a5c5a9f9e78b91f1da9a605fe43",
+    "url": "/My-Portfolio/static/media/jpheart.f7f33a5c.png"
+  }
+]);
