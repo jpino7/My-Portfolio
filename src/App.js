6925eb28682d5100b48de2d3de3bdb5b7b3eccae
JRPino <pino3789@gmail.com>
2020-07-03 11:03:11 -0700

    Issues with routing homepage

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           <Nav />
           <Header />
           <Switch>
+            <Route exact path="/My-Portfolio" component={About} />
             <Route exact path="/" component={About} />
             <Route exact path="/portfolio" component={Portfolio} />
             <Route exact path="/contact" component={Contact} />
