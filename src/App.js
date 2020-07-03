1e3f1e17aeac21b21ec7fe9fb50ce6febec924c1
Joanna Pino <pino3789@gmail.com>
2020-07-03 10:07:19 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Nav />
           <Header />
           <Switch>
-            <Route exact path="/" component={About} />
+            <Route exact path="/about" component={About} />
             <Route exact path="/portfolio" component={Portfolio} />
             <Route exact path="/contact" component={Contact} />
           </Switch>
