10e1ea6cac328cf92a07edd6f8c5882c2f78a3ba
JRPino <pino3789@gmail.com>
2020-07-28 11:02:11 -0700

    took out baseline

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Nav />
           <Header />
           <Switch>
-            <BrowserRouter baseline="/My-Portfolio" />
+            {/* <BrowserRouter baseline="/My-Portfolio" /> */}
             <Route exact path ="/My-Portfolio" component={About}/>
             <Route exact path="/" component={About} />
             <Route exact path="/portfolio" component={Portfolio} />
